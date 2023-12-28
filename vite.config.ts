98ecd7357251b1b890d7d7ff05463842235e3cd8
muhashi <105213357+muhashi@users.noreply.github.com>
2023-12-29 09:49:30 +1000

    Fix building

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   // depending on your application, base can also be "/"
-  base: '',
+  base: '/', // https://vitejs.dev/guide/static-deploy.html#github-pages
   plugins: [react()],
   server: {
     // this ensures that the browser opens upon server start
