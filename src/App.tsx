0bc86d9993e429ff17ecc9d1818b5138ae3de845
muhashi <105213357+muhashi@users.noreply.github.com>
2024-06-24 06:12:29 +1000

    Add popup for update release notes

--- src/App.tsx
+++ src/App.tsx
@@ -296,6 +296,37 @@ function Header() {
   );
 }
 
+function UpdatePopup() {
+  const seenUpdate = Cookies.get('seenUpdate');
+  const [open, setOpen] = useState(!seenUpdate);
+
+  const handleClose = () => {
+    Cookies.set('seenUpdate', '1', { expires: 500 });
+    setOpen(false);
+  };
+
+  return (
+    <Fragment>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Zoom}
+        fullWidth={true}
+      >
+        <Box sx={{
+          display: 'flex', flexDirection: 'column', alignContent: 'center', justifyContent: 'center', alignItems: 'center', width: '85%', minHeight: '10rem', gap: '1rem', margin: '2rem auto',
+        }}>
+          <StyledTypography variant='h5'>New Update!</StyledTypography>
+          <StyledTypography variant='body1'>Thank you for your patience, the new Geodle update is finally here!</StyledTypography>
+          <StyledTypography variant='body1'>We have increased the number of countries to <b>181 countries</b></StyledTypography>
+          <StyledTypography variant='body1'>Enjoy a more complete and difficult Geodle { ":)" }</StyledTypography>
+          <StyledTypography variant='body1'>Don't hesitate to email us any bugs!</StyledTypography>
+        </Box>
+      </Dialog>
+    </Fragment>
+  );
+}
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
@@ -306,6 +337,7 @@ function App() {
         >
           <Header />
           <Main />
+          <UpdatePopup />
         </Box>
       </div>
     </ThemeProvider>
